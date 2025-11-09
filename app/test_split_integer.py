a07d8e2ff5184b5fbfebb46bf819a13e7711c147
Vadym Vynokur <king.krollie@gmail.com>
2025-11-09 18:38:38 +0200

    remove last test

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -19,7 +19,3 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(10, 11) == [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-
-def test_split_32_into_6_parts() -> None:
-    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
